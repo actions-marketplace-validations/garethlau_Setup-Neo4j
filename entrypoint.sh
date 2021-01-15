32827ddf3459cabdd4e53a1eddef167a773b6470
Gareth Lau <43855680+garethlau@users.noreply.github.com>
2021-01-14 20:36:16 -0700

    Bind mount volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 -p 7687:7687  -e NEO4J_AUTH=neo4j/$INPUT_PASSWORD -e NEO4JLABS_PLUGINS=\[\"apoc\"\] neo4j:$INPUT_TAG"
+docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 -v $PWD/data:/data -v $PWD/plugins:/plugins -e NEO4J_AUTH=neo4j/$INPUT_PASSWORD -e NEO4JLABS_PLUGINS=\[\"apoc\"\] neo4j:$INPUT_TAG"
 
 sh -c "$docker_run"
